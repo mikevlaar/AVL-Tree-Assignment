98f6b1dd9a7ca6ba9160fc72dbd9e2121d59a67b
mikevlaar <udgement1>
2014-02-16 22:25:24 +0100

    now with node

---
+++ AVL Tree/AVL Tree/AvlNode.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class AvlNode 
+{
+    public AvlNode left;
+    public AvlNode right;
+    public AvlNode parent;
+    public int key;
+    public int balance;
+
+    public AvlNode(int k) 
+    {
+        left = right = parent = null;
+        balance = 0;
+        key = k;
+    }
+}
